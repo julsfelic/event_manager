5ebfe2c40b7a1e190b0dd9325f2196e060a0b10b
Julian Feliciano <julsfelic@gmail.com>
2016-01-09 18:59:14 -0700

    Switch over to the CSV library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,11 @@
+require "csv"
 puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv" if File.exists? "event_attendees.csv"
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open "event_attendees.csv", headers: true,
+                                           header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
   puts name
 end
